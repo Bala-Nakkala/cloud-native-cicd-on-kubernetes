ffff1d2c13eb53ac0a77c127b5dfadc1780697f8
Balu@N <balakrishna.nakkala@gmail.com>
2026-07-23 10:06:23 +0000

    Added professional DevOps dashboard

--- app/src/main/java/com/balu/springboot_demo/HelloController.java
+++ app/src/main/java/com/balu/springboot_demo/HelloController.java
@@ -1,21 +1,13 @@
 package com.balu.springboot_demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-
-    @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "🚀 Hello from DevOps CI/CD Pipeline!";
     }
-
 }
